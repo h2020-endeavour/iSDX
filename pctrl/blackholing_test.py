85318a5e45d09ac45f03055470e9d68664100931
Florian Kaufmann <florian.kaufmann@de-cix.net>
2016-08-08 10:36:54 +0200

    update json test file for blackholing

--- pctrl/blackholing_test.py
+++ pctrl/blackholing_test.py
@@ -1,69 +1,15 @@
 {
-  "policy": [
-    {
-      "removal_cookies": [
-        {
-          "cookie": 57,
-          "match": {
-            "tcp_dst": 1111
-          },
-          "action": {
-            "fwd": 0
-          }
-        },
-
-        {
-          "cookie": 58,
-          "match": {
-            "tcp_dst": 1111
-          },
-          "action": {
-            "fwd": 1
-          }
-        },
-
-        {
-          "cookie": 59,
-          "match": {
-            "tcp_dst": 1111
-          },
-          "action": {
-            "fwd": 1
-          }
+    "inbound": [
+         {
+            "cookie": 3,
+            "match": 
+            {
+                "eth_src": "08:00:27:89:3b:9f"
+            },
+            "action": 
+            {
+                "drop": 0
+            }
         }
-
-      ]
-    },
-    {
-      "new_policies": [
-        {
-          "cookie": 1,
-          "match": {
-            "tcp_dst": 4323
-          },
-          "action": {
-            "fwd": 0
-          }
-        },
-        {
-          "cookie": 2,
-          "match": {
-            "tcp_dst": 4324
-          },
-          "action": {
-            "fwd": 1
-          }
-        },
-        {
-          "cookie": 3,
-          "match": {
-            "eth_src": "08:00:27:89:3b:9f"
-          },
-          "action": {
-            "drop": 0
-          }
-        }
-      ]
-    }
-  ]
+    ]
 }
