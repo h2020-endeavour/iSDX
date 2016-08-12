f3bef1d2bd1c9b7c4c4966d6a701ff14bdc4f033
Florian Kaufmann <florian.kaufmann@de-cix.net>
2016-08-12 13:29:30 +0200

    update blackholing config file

--- pctrl/blackholing_test.py
+++ pctrl/blackholing_test.py
@@ -1,17 +1,19 @@
 {
   "policy": [
     
-
     {
       "remove": [
         
-
         {
           "inbound": [
             {
               "cookie": 3,
-              "match": { "eth_src": "08:00:27:89:3b:9f" },
-              "action": { "drop": 0 }
+              "match": {
+                "eth_src": "08:00:27:89:3b:9f"
+              },
+              "action": {
+                "drop": 0
+              }
             }
           ]
         }
@@ -20,13 +22,12 @@
       ]
     },
     
-
     {
       "insert": [
         
-
         {
           "inbound": [
+            
             {
               "cookie": 3,
               "match": {
@@ -42,7 +43,7 @@
 
       ]
     }
-
+  
 
   ]
 }
